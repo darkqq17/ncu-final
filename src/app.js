bd28837bb37ef564fd29f39718aa77f486dae4e3
darkqq17 <b0983729632@gmail.com>
2020-10-15 15:50:34 +0800

    fix: resolving cors policy

--- src/app.js
+++ src/app.js
@@ -1,20 +1,16 @@
 const koa = require('koa');
-const router = require('koa-router');
 const logger = require('koa-logger');
 const koaBody = require('koa-bodyparser');
 const apiRouter = require('./router/router');
 const cors = require('@koa/cors');
 
 const app = new koa();
-// const host = process.env.HOST || '172.20.10.11';
 const PORT = process.env.PORT || 3000;
 
-// request -> node.js tcp socket(http) -> koa middlewares
 app.use(logger());
+app.use(cors());
 app.use(koaBody());
 app.use(apiRouter.routes());
-// Open a server instance
-app.use(cors()); // 跨域
 
 app.listen(PORT, () => {
     console.log(`[SERVER] is listening on port ${PORT}`)
